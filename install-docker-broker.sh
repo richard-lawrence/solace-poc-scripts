4eb2deef9257f127c48b45ec13ebdce00ae716dc
Richard Lawrence <56631278+richard-lawrence@users.noreply.github.com>
2021-11-18 11:29:41 +0000

    Update install-docker-broker.sh

--- install-docker-broker.sh
+++ install-docker-broker.sh
@@ -36,6 +36,6 @@ fi
 
 echo "Installing image: $IMAGE:$TAG"
 
-# Port 55555 is blocked on Macs so use 55556
+# If on Mac Port 55555 is blocked so change to use 55556
 
-docker run -d -p 8080:8080 -p 55556:55555 -p 55443:55443 -p 1943:1943 -p 9443:9443 -p 55003:55003 -p 8008:8008 -p 1443:1443 -p 1883:1883 -p 8000:8000 -p 8883:8883 -p 8443:8443 -p 5671:5671 -p 5672:5672 -p 9000:9000 -p 2222:2222 --shm-size=2g --env username_admin_globalaccesslevel=admin --env username_admin_password=admin --env service_semp_tlsport=1943 --env system_scaling_maxconnectioncount=1000 --cpus=2 --name=$NAME $IMAGE:$TAG
+docker run -d -p 8080:8080 -p 55555:55555 -p 55443:55443 -p 1943:1943 -p 9443:9443 -p 55003:55003 -p 8008:8008 -p 1443:1443 -p 1883:1883 -p 8000:8000 -p 8883:8883 -p 8443:8443 -p 5671:5671 -p 5672:5672 -p 9000:9000 -p 2222:2222 --shm-size=2g --env username_admin_globalaccesslevel=admin --env username_admin_password=admin --env service_semp_tlsport=1943 --env system_scaling_maxconnectioncount=1000 --cpus=2 --name=$NAME $IMAGE:$TAG
